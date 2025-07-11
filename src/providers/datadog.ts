f532a70a89200830df38498dbfcbd959c0147d47
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2025-07-11 11:08:51 -0300

    Update datadog package and fix configuration

--- src/providers/datadog.ts
+++ src/providers/datadog.ts
@@ -1,21 +1,19 @@
-import { datadogRum } from '@datadog/browser-rum';
+import { datadogLogs } from '@datadog/browser-logs';
 import { LogEntry } from '../monitor';
 
-export const initDatadog = ({ token, env }: { token?: string; env?: string }) => {
-  datadogRum.init({
-    applicationId: token!,
+type Site = 'datadoghq.com' | 'us3.datadoghq.com' | 'us5.datadoghq.com' | 'datadoghq.eu' | 'ddog-gov.com' | 'ap1.datadoghq.com';
+export const initDatadog = ({ token, env, site, service }: { token?: string; env?: string, site?: Site, service?: string }) => {
+  datadogLogs.init({
     clientToken: token!,
-    site: 'datadoghq.com',
-    service: 'frontend',
+    site: site || 'us5.datadoghq.com',
+    service: service || 'frontend',
     env,
     sessionSampleRate: 100,
-    sessionReplaySampleRate: 100,
-    trackUserInteractions: true,
   });
 };
 
 export const datadogLogger = (entry: LogEntry) => {
-  datadogRum.addError(entry.message, {
+  datadogLogs.logger[entry.level](entry.message, {
     tags: entry.tags?.join(',') || '',
     ...entry.customProperties,
     level: entry.level,
