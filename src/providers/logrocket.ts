c8db8a14545b5a0c3645e4cae41b852c442ba5d8
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2025-07-14 22:22:21 -0300

    Update logrocket.ts

--- src/providers/logrocket.ts
+++ src/providers/logrocket.ts
@@ -1,16 +1,18 @@
 import LogRocket from 'logrocket';
 
-export const initLogRocket = ({ token, environment }: { token: string; environment?: string }) => {
+export const initLogRocket = ({ token, environment, trackErrors }: { token: string; environment?: string, trackErrors?: boolean }) => {
   LogRocket.init(token, {
     release: environment,
+    console: {
+      isEnabled: trackErrors !== undefined ? trackErrors : true,
+    }
   });
 }
 
-export type LogLevel = 'debug' | 'info' | 'warn' | 'error' | 'critical';
+export type LogLevel = 'debug' | 'info' | 'warn' | 'error';
 
 export const logRocketLogger = (entry: { level: LogLevel; message: string; logProperties?: Record<string, any> }) => {
-  LogRocket.log(entry.message, {
+  LogRocket[entry.level as LogLevel](entry.message, {
     ...entry.logProperties,
-    level: entry.level,
   });
 };
